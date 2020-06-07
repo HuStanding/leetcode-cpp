403b1d110e65bc46868aa95d511db0fc22c2b7b0
HustElvan <1099270667@qq.com>
2020-06-07 14:55:05 +0800

    init

---
+++ bi-search/code_69_sqrt.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;   
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x <= 1){
+            return x;
+        }
+        // 使用二分查找算法
+        int low = 1;
+        long int high = x;
+        while(low <= high){
+            long int mid = (low + high) >> 1;
+            if(mid == x / mid) return mid;
+            else if (mid < x /mid) low = mid + 1;
+            else  high = mid - 1;
+        }
+        return high;
+    }
+};
+
+
+int main(){
+    Solution test = Solution();
+    cout << test.mySqrt(2147483647) << endl;
+}
